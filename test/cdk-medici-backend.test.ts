09bbd399081b7d934ad99d7327dd4048e9c56176
ovaldezb <omar.valdez.becerril@gmail.com>
2023-05-12 09:49:58 -0600

    Initial commit

---
+++ test/cdk-medici-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMediciBackend from '../lib/cdk-medici-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-medici-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMediciBackend.CdkMediciBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
